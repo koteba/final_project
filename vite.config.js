4fc90f64bdd45f865a4f2a6e879fc33f9eb9f18b
koteba <koteba.ali98@gmail.com>
2025-01-29 09:48:29 +0300

    final_project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/final_project/",
+
   plugins: [react()],
   server: {
     port: 3000, // تغيير المنفذ الافتراضي إلى 3000
